f4a4ed0ba08de70155a48440b1588e51f76b6f5d
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2023-04-28 14:36:03 +0300

    Add files via upload
    Soru 2 (30P): 
    a) Giriş parametresi olarak adı (d) olan int tipinde tek boyutlu bir dizi ve adı (a) olan int tipinde bir 
    değişken alan, (a) değişkenini (d) dizisi içinde İkili Arama (Binary Search) yöntemine göre arayan ve 
    aranan değişken varsa dizideki indis numarasını yoksa -1 değerini geriye cevap olarak döndüren bir 
    fonksiyon yazınız (Parametre olarak verilen (d) dizisinin sıralı olup olmadığı bilinmemektedir). 
    b) Main fonksiyonu içinde ekrandan dizinin kapasite bilgisi ve aranacak eleman bilgisini int tipinde 
    alınız. Belirtilen kapasitede tek boyutlu bir int dizi tanımlayınız. Bu dizinin içine 10-100 arasında (10 
    ve 100 dahil olmak üzere) rastgele sayılar atayınız. (a) maddesinde oluşturduğunuz fonksiyonu 
    kullanarak aranacak elemanı dizide arayarak, fonksiyondan gelen cevaba göre uygun mesajı ekrana 
    yazdırınız

--- Program.cs
+++ Program.cs
@@ -4,15 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace soru1
+namespace soru2
 {
     internal class Program
     {
+        public static int ikiliArama(int[] d, int a)
+        {
+            int sol = 0;
+            int sag = d.Length - 1;
+            while (sol <= sag)
+            {
+                int orta = (sag + sol) / 2;
+                if (d[orta] == a)
+                {
+                    return orta;
+                }
+                else if (d[orta] < a)
+                {
+                    sol = orta + 1;
+                }
+                else
+                {
+                    sag = orta - 1;
+                }
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Elif");
-            Console.WriteLine("Ozun");
-            Console.WriteLine("210401051");
+            /*int[] dizi = { 1, 2, 3, 4, 5, 6 };
+            int deneme = ikiliArama(dizi, 9);
+            Console.Write(deneme);*/
+
+            Console.WriteLine("dizinin kapasitesini giriniz.");
+            int diziKapasite = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("aranacak elemanı giriniz.");
+            int arananEleman = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int[] dizi = new int[diziKapasite];
+
+            for (int i = 0; i < diziKapasite; i++)
+            {
+                dizi[i] = rnd.Next(10, 101);
+            }
+            Array.Sort(dizi);//binary search yapmak icin gerekli.
+
+            int result = ikiliArama(dizi, arananEleman);
+
+            if (result == -1) Console.WriteLine("aradığınız sonuç dizide bulunmamaktadır.");
+            else Console.WriteLine("girdiğiniz " + arananEleman + " sayısı dizide su indiste bulunmustur: " + result);
             Console.ReadKey();
         }
     }

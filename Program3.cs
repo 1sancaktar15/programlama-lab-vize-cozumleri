d975eba95dc5bc3a21fc7bc19d08ff4368fc19f4
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2023-04-28 14:37:32 +0300

    Add files via upload
    Soru 3 (30P): 
    a) Giriş parametresi olarak adı (x) ve (n) adlarında iki adet int tipinde değişken alan, buna göre 𝑥𝑥𝑛𝑛
    değerini hesaplayan ve cevabı geriye döndüren yinelemeli (recursive) bir fonksiyon yazınız.
    b) Main fonksiyonu içinde ekrandan int tipinde (a) ve (b) adlarında iki tamsayı alınız. (a) maddesinde 
    oluşturduğunuz fonksiyonu kullanarak 𝑎𝑎𝑏𝑏 değerini hesaplayarak sonucu ekrana yazdırınız

---
+++ Program3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru3
+{
+    internal class Program
+    {   
+        public static int x_ussu_n_rec(int x , int n)
+        {
+            if (n == 0) return 1;
+            return x_ussu_n_rec(x , n -1) * x ;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ussu alinacak elemani giriniz.");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ussu alinacak eleman kac kere carpilacak giriniz.");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x_ussu_n_rec(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}

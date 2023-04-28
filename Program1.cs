fa30386519a7fa869aa884a7f40cba588abd2daa
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2023-04-28 14:38:29 +0300

    Add files via upload
    Soru 1 (10P):
    Ekrana adınızı, soyadınızı ve okul numaranızı alt alta yazdırınız

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elif");
+            Console.WriteLine("Ozun");
+            Console.WriteLine("210401051");
+            Console.ReadKey();
+        }
+    }
+}

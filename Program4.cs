6efb7f410259a8e5a3663df4b915ae6adca867ac
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2023-04-28 14:39:33 +0300

    Add files via upload
    Soru 4 (30P): 
    a) Giriş parametresi olarak adı (x) olan bir string değişken alan, uygun bir koleksiyon yapısında (x) 
    ifadesi içinde geçen harfleri ve her harfin kaç kere geçtiğini tutan ve bu koleksiyonu geriye cevap 
    olarak döndüren bir fonksiyon yazınız.
    Örneğin x=”merhaba” ise koleksiyonun içinde m:1, e:1, r:1, h:1, a:2, b:1 şeklinde bilgiler tutulacak.
    b) Main fonksiyonu içinde ekrandan string tipinde (a) adında bir ifade alınız. (a) maddesinde 
    oluşturduğunuz fonksiyonu kullanarak bu ekrandan girilen (a) değişkeni içindeki harf ve her harfin 
    kaç kere geçtiğinin sayısını ekrana yazdırınız.

---
+++ Program4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru4
+{
+    internal class Program
+    {
+        static Dictionary<char, int> CharacterSay(string x)
+        {
+            var charSayaci = new Dictionary<char, int>();
+
+            foreach (char eleman in x)
+            {
+                if (charSayaci.ContainsKey(eleman))
+                {
+                    charSayaci[eleman]++;
+                }
+                else
+                {
+                    charSayaci.Add(eleman, 1);
+                }
+            }
+            return charSayaci;
+        }
+
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter a string: ");
+            string a = Console.ReadLine();
+            Dictionary<char, int> charCounts = CharacterSay(a);
+
+            foreach (KeyValuePair<char, int> pair in charCounts)
+            {
+                Console.Write("{0}:{1}, ", pair.Key, pair.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
